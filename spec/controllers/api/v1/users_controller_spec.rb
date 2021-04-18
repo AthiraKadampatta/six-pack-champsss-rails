0f6549c4584f283587b7d5b891332c60731a4354
AthiraKadampatta <athira@kiprosh.com>
2021-04-18 18:17:13 +0530

    #10 refactor expectation in specs

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,14 +1,28 @@
-require 'spec_helper'
 require 'rails_helper'
 
 describe Api::V1::UsersController, type: :request do
-
   describe 'show' do
-    it 'should have required keys in response' do
-      user = users(:one)
-      get "/api/v1/users/#{user.id}"
+    let(:user) { users(:one) }
+    subject(:show_users_api) { get "/api/v1/users/#{user.id}" }
 
+    it 'returns appropriate headers' do
+      show_users_api
       expect(response.content_type).to include("application/json")
+    end
+
+    it 'has user and total points in response' do
+      show_users_api
+      expect(json_response.keys).to include(:user, :total_points)
+    end
+
+    it 'has required keys in user' do
+      show_users_api
+      expect(json_response[:user].keys).to include(:id, :name, :token, :email, :role_id)
+    end
+
+    it 'has required keys in total_points' do
+      show_users_api
+      expect(json_response[:total_points].map(&:keys).flatten.uniq).to include(:project_name, :points)
     end
   end
 end

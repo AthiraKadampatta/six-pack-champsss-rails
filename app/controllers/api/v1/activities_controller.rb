74da94d8b2a3096a964c897de8efc2e9dd2afd45
Prayesh Shah <Prayesh@kiprosh.com>
2021-04-25 17:32:29 +0530

    adds apipie for actions

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,9 +1,22 @@
 class Api::V1::ActivitiesController < ApplicationController
   before_action :set_activity, only: [:update, :destroy]
 
+  api :GET, 'v1/activities', 'Lists all the activities of a user'
+  param :status, String, desc: 'pending, approved or rejected', required: false
+
   def index
-    @activities = @current_user.activities.where(status: params[:status])
+    @activities = @current_user.activities
+    @activities = @activities.where(status: params[:status]) if params[:status]
     render json: @activities
+  end
+
+  api :POST, 'v1/activities', 'Creates a user activity'
+  param :activity, Hash, desc: 'Activity info' do
+    param :description, String, desc: 'Description of the activity', required: true
+    param :duration, Integer, desc: 'Activity duration in minutes', required: true
+    param :project_id, Integer, desc: 'ID of the project', required: true
+    param :points_requested, Integer, desc: 'Points requested by the user', required: true
+    param :performed_on, Date, desc: 'Date on which the activity is performed', required: true
   end
 
   def create
@@ -15,6 +28,16 @@ class Api::V1::ActivitiesController < ApplicationController
     end
   end
 
+  api :PUT, 'v1/activities/:id', 'Updates "pending" activity only'
+  param :id, Integer, required: true
+  param :activity, Hash, desc: 'Activity info' do
+    param :description, String, required: true
+    param :duration, Integer, desc: 'Activity duration in minutes', required: true
+    param :project_id, Integer, required: true
+    param :points_requested, Integer, desc: 'Points requested by the user', required: true
+    param :performed_on, Date, required: true
+  end
+
   def update
     return head :not_found unless @activity
 
@@ -24,6 +47,9 @@ class Api::V1::ActivitiesController < ApplicationController
       render json: @activity.errors, status: :unprocessable_entity
     end
   end
+
+  api :DELETE, 'v1/activities/:id', 'Deletes "pending" activity only'
+  param :id, Integer, required: true
 
   def destroy
     return head :not_found unless @activity

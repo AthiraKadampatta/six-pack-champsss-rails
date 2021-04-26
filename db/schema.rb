7f8cadf5e535efd2be933b725dc725fde3734f3a
Prayesh Shah <Prayesh@kiprosh.com>
2021-04-26 19:51:18 +0530

    adds points calculation logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_132637) do
+ActiveRecord::Schema.define(version: 2021_04_25_163822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2021_04_24_132637) do
     t.integer "transactable_id"
     t.string "transactable_type"
     t.integer "points"
-    t.integer "type"
+    t.integer "txn_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

7400e69752a6db116fbb642a65ef350fa11c56e1
Prayesh Shah <Prayesh@kiprosh.com>
2021-04-29 21:58:19 +0530

    Refactor sessions#login action
    # Conflicts:
    #	db/schema.rb

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,13 +4,9 @@ class Api::V1::SessionsController < ApplicationController
   api :POST, '/v1/auth/login', 'Create session and user for login'
   param :id_token, String, description: 'id_token returned by Google auth api', required: true
   def login
-    validator = GoogleIDToken::Validator.new
     begin
-      token = params[:id_token]
-      aud = JWT.decode(token, nil, false)[0]['aud']
-      payload = validator.check(token, aud)
-
-      user = User.find_or_create_by(email: payload['email'])
+      payload = GoogleValidatorService.new(params[:id_token]).call
+      user = fetch_user_by_email(payload['email'], params)
 
       render json: {
         access_token: JsonWebTokenService.encode({ email: user.email }),
@@ -19,6 +15,18 @@ class Api::V1::SessionsController < ApplicationController
 
     rescue GoogleIDToken::ValidationError => e
       head :unauthorized
+    end
+  end
+
+  private
+
+  def fetch_user_by_email(email, params)
+    User.find_or_initialize_by(email: email).tap do |u|
+      if u.new_record?
+        u.name = params[:name]
+        u.image_url = params[:image]
+        u.save!
+      end
     end
   end
 end
---
+++ app/lib/google_validator_service.rb
@@ -0,0 +1,21 @@
+class GoogleValidatorService
+  attr_reader :id_token
+  attr_accessor :validator
+
+  def initialize(id_token)
+    @id_token = id_token
+    @validator = GoogleIDToken::Validator.new
+  end
+
+  def call
+    return false unless @id_token
+    validate
+  end
+
+  private
+
+  def validate
+    aud = JWT.decode(@id_token, nil, false)[0]['aud']
+    payload = @validator.check(@id_token, aud)
+  end
+end

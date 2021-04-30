98cbe9535144b8f8619e08b2a6a0721468f4d3d8
SupriyaMedankar <supriya.medankar@kiprosh.com>
2021-04-30 10:15:55 +0530

    fix weekly query

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -42,14 +42,15 @@ class Activity < ApplicationRecord
   end
 
   def self.weekly_top_five_contibutors
+    prev_week_start_date = 1.week.ago.to_date
     @weekly_top_five_contibutors ||=
       ActiveRecord::Base.connection
-        .exec_query('SELECT users.name, sum(activities.points_granted) as total_points
-        FROM "users" INNER JOIN "activities"
-        ON "activities"."user_id" = "users"."id"
-        WHERE "activities"."status" = 1
-        GROUP BY "users"."id"
-        ORDER BY "total_points" DESC;').rows
+        .exec_query("SELECT users.name, sum(activities.points_granted) as total_points
+        FROM \"users\" INNER JOIN \"activities\"
+        ON \"activities\".\"user_id\" = \"users\".\"id\"
+        WHERE \"activities\".\"status\" = 1 AND \"activities\".\"reviewed_at\" >= '#{prev_week_start_date}'
+        GROUP BY \"users\".\"id\"
+        ORDER BY \"total_points\" DESC;").rows
   end
 
   def self.slack_notification_message
